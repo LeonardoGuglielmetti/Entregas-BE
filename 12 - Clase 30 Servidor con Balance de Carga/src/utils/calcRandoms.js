7f8defbb65e85b49cd2787843228805b80502eb9
Leonardo Guglielmetti <106181182+LeonardoGuglielmetti@users.noreply.github.com>
2023-03-09 20:00:36 -0300

    Usando el Objeto Proces

---
+++ 12 - Clase 30 Servidor con Balance de Carga/src/utils/calcRandoms.js
@@ -0,0 +1,25 @@
+export function calcRandoms(param) {
+    let result = {};
+    for (let i = 0; i < param; i++) {
+        const randomNum = Math.round(Math.random() * 1000);
+        if (!result[randomNum]) {
+            result[randomNum] = 1
+        } else {
+            result[randomNum]++
+        }
+    }
+    return result;
+};
+/* 
+process.on('message', (param) => {
+    let result = {};
+    for (let i = 0; i < param; i++) {
+        const randomNum = Math.round(Math.random() * 1000);
+        if (!result[randomNum]) {
+            result[randomNum] = 1
+        } else {
+            result[randomNum]++
+        }
+    }
+    process.send(result);
+}); */
